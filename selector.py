507ad5dc3ab9bd9f0ce5d0145235a83f10475edd
Thiago Guedes <sradtsor@gmail.com>
2022-05-29 12:18:24 -0300

    First try on Python

---
+++ selector.py
@@ -0,0 +1,19 @@
+import random
+
+friends = [
+    'Pedro',
+    'Camila',
+    'Laura',
+    'Marcos',
+    'Kevin',
+    'Ana',
+    'Suzana',
+    'Amanda',
+    'Nayara'
+]
+
+
+selected = random.choice(friends) 
+
+print('Com quem farei um FaceTime hoje?')
+print(selected)
